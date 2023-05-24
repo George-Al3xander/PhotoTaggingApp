77353517002edd932fb90febd0692f4656b8a153
George <george.valuiskyi@gmail.com>
2023-05-24 15:14:21 +0300

    Quick commit(timer added)

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -2,16 +2,18 @@ import React from "react";
 
 function DropdownMenu(props) {   
     return(
-      <ul 
-        style={{
+
+        <div style={{
             left: props.styleLeft + "%", 
-            top: props.styleTop + "%"
-        }} className="dropdown-menu">
-        <div className="crosshair"></div>
+            top: props.styleTop + "%",            
+        }}  className="dropdown-menu">
+        {/* <div className="crosshair"></div> */}
+        <ul>
         <li onClick={props.menuClickFunc}>Red Hulk</li>
         <li onClick={props.menuClickFunc}>Vision</li>
         <li onClick={props.menuClickFunc}>Ironfist</li>
-      </ul>
+        </ul>
+        </div>
     )
 }
 
